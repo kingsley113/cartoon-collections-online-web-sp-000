343b489c76fc6d3cbf9cffd074385cd63b7fea55
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-16 23:04:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,3 +14,6 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+test_names = [Amy, Bob, Charlie, Dave, Eric, Frank]
+roll_call_dwarves(test_names)
