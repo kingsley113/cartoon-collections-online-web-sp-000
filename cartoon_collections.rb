0a533d168638c26d8f70bc13a363b4b535072998
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-17 01:51:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,10 +11,18 @@ def long_planeteer_calls(calls)
   calls.any? { |x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?(cheese_types)
+    return cheese_types
+  else
+    nil  
+  # the array below is here to help
+  
 end
+
+
+
 
 test_names = ["amy", "bob", "charlie", "dave", "eric", "frank"]
 summon_captain_planet(test_names)
